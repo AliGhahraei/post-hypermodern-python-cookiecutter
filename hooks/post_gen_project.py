f867c235f6f300acc5f4cacec2c3999fad32148f
Ali Ghahraei <aligf94@gmail.com>
2025-06-04 01:24:14 -0700

    Migrate to subprocess.run to have checks

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,4 +1,4 @@
-import os
+from subprocess import run
 from pathlib import Path
 
 PROJECT_DIRECTORY = Path().cwd()
@@ -20,9 +20,9 @@ def initialize_git():
     """Initializes a git repository and makes an initial commit."""
     if not (PROJECT_DIRECTORY / ".git").exists():
         emphasize("Initializing git repository...")
-        os.system("git init")
-        os.system("git add .")
-        os.system('git commit -m "Initial commit from cookiecutter template"')
+        run("git init", shell=True, check=True)
+        run("git add .", shell=True, check=True)
+        run('git commit -m "Initial commit from cookiecutter template"', shell=True, check=True)
         print("Git repository initialized.")
 
 
@@ -30,8 +30,8 @@ def setup_project():
     """Sets up project dependencies and git hooks using mise."""
     emphasize("Setting up project with mise (installing dependencies and git hooks)...")
     warning("NOTE: this will also trust the mise config for your project: https://mise.jdx.dev/cli/trust.html")
-    os.system("mise trust .")
-    os.system("mise run setup")
+    run("mise trust .", shell=True, check=True)
+    run("mise run setup", shell=True, check=True)
     print("Project setup complete.")
 
 
